12c84c7dfb57f70f08bd5bce275132233b7c873f
Tatenda Marvelous Chimusoro <52354549+devTatendaMarvelous@users.noreply.github.com>
2025-08-28 08:05:17 -0700

    Add remaining category CRUD views, layout, and update routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
-<?php
+use App\Http\Controllers\AssetController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AssignmentController;
+use App\Http\Controllers\AssetLogController;
 
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('categories', CategoryController::class);
+Route::resource('assets', AssetController::class);
+Route::resource('assignments', AssignmentController::class);
+Route::resource('asset_logs', AssetLogController::class);
